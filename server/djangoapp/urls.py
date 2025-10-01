49466e45d14276795609eb0b59520ac002dff47d
sc-freelance <saadchughtai756@gmail.com>
2025-10-01 08:55:43 -0400

    Added register and logout functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,8 +6,9 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
+    # path for registration
+    path(route='registration', view=views.registration, name='registration'),
+    
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
