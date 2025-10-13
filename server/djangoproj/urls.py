daba7c001436a142c2d57f4fff34ffad1bf7ea16
sc-freelance <saadchughtai756@gmail.com>
2025-10-13 15:23:33 -0400

    update for blank page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
-
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
